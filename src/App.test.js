444bd8079185f227671013b5723391e25df1b585
Dylan Stone-Miller <stonemiller.dylan@gmail.com>
2021-07-06 10:52:52 -0400

    testing workflow on main branch

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/belch/i);
+  const linkElement = screen.getByText(/woah/i);
   expect(linkElement).toBeInTheDocument();
 });
